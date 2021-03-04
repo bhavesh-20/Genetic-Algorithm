64b89707d82d92f1a7305760c434dd760827c26b
Bhavesh <bhavesh189kumar@gmail.com>
2021-03-04 19:59:14 +0530

    Moidified crossover and added comments

--- Sudoku.py
+++ Sudoku.py
@@ -1,6 +1,9 @@
 import numpy as np
 
 
+"""Function which generates random strings for genetic algorithm, each string
+ is a representation of genetic string used in the algorithm."""
+
 def random_generation():
     l = ["x8xxxxx9x","xx75x28xx","6xx8x7xx5","37xx8xx51","2xxxxxxx8","95xx4xx32","8xx1x4xx9","xx19x36xx",
         "x4xxxxx2x"]
@@ -22,11 +25,18 @@ def random_generation():
     return rand_string
 
 
+"""
+The fitness function is based on how fit the string is based on rows,columns and blocks of sudoku.
+The fitness calculated is sum of the number of distinct numbers in each row, column or block.
+The solution has the fitness of (9*9) + (9*9) + (9*9) = 81 * 3 = 243, which is the highest fitness possible
+"""
+
 def fitness_function(rand_string :str) -> int:
     rand_string = [rand_string[(x-1)*9:x*9] for x in range(1,10)]
     return row_fitness(rand_string) + column_fitness(rand_string) + block_fitness(rand_string)
     pass
 
+#Part of fitness function: calculates Fitness of the rows
 
 def row_fitness(rand_string):
     s = 0
@@ -34,12 +44,16 @@ def row_fitness(rand_string):
         s += len(set(i))
     return s
 
+#Part of fitness function: calculates Fitness of the columns
+
 def column_fitness(rand_string):
     columns = ['']*9
     for i in rand_string:
         for j in range(9):
             columns[j] += i[j]
     return row_fitness(columns)            
+
+#Part of fitness function: calculates Fitness of the Blocks
 
 def block_fitness(rand_string):
     blocks = ['']*9
@@ -51,11 +65,22 @@ def block_fitness(rand_string):
     return row_fitness(blocks)
 
 
+#crossover for strings
+""" Here crossover takes place between two strings, and crossover point is the midpoint of both strings """
+
 def crossover_1(string1 :str, string2 :str) -> list:
     string1, string2 = string1[:40] + string2[40:], string2[:40] + string1[40:]
     return [string1,string2]
 
-def crossover_2(string1 :list, string2 :list) -> list:
-    string1, string2 = string1[:4] + string2[4:], string2[:4] + string1[4:]
 
+#crossover for list of strings
+"""here crossover takesplace with rows, that is, the random point for crossover is always 
+    the ending point of a row or starting point of a row, in this case starting point of 5th row"""
+
+def crossover_2(string1 :str, string2 :str) -> list:
+    string1 = [string1[(x-1)*9:x*9] for x in range(1,10)]
+    string2 = [string2[(x-1)*9:x*9] for x in range(1,10)]
+    string1, string2 = string1[:4] + string2[4:], string2[:4] + string1[4:]
+    string1, string2 = ''.join(x for x in string1),  ''.join(x for x in string2)
+    return [string1,string2]
 
